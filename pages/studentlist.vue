9b4890e4d42ff51ed9dd75d0c6e82d321af6072e
JoTolentino0219 <joantolentino0219@gmail.com>
2024-05-09 11:50:21 +0800

    added my project

---
+++ pages/studentlist.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-data-table
+      :headers="headers"
+      :items="desserts"
+      :items-per-page="5"
+      class="elevation-1"
+    ></v-data-table>
+  </template>
+  
+  <script>
+  export default {
+    data () {
+      return {
+        headers: [
+          {
+            text: 'Dessert (100g serving)',
+            align: 'start',
+            sortable: false,
+            value: 'name',
+          },
+          { text: 'Student', value: 'Student' },
+          { text: 'Lastname', value: 'Lastname' },
+          { text: 'Firstname', value: 'Firstname' },
+          { text: 'Middlename', value: 'Middlename' },
+          { text: 'Course', value: 'Course' },
+          {text: 'Section', value: 'Section' },
+
+        ],
+        desserts: [ ],
+      }
+    },
+  }
+</script>
